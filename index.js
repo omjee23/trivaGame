8bfa70cdbb0afa0170d3f5407698f07153ddbafb
OM JEE PAL <141926572+omjee23@users.noreply.github.com>
2024-10-06 15:52:18 +0530

    add path

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const mongoose = require('mongoose');
 const axios = require('axios');
 const cors = require('cors');
 const routes = require('./routes/api.router');
+const path = require('path');
 const port = 3000;
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'front-end')));
 app.use(express.json());
 app.use('/', routes)
 
 app.get('/', (req, res) => {
-    res.send(JSON.stringify("Welcome to trivdata : Mr Om"));
-})
+    res.sendFile(path.join(__dirname, 'front-end', 'index.html'));
+});
+
+// app.get('/', (req, res) => {
+//     res.send(JSON.stringify("Welcome to trivdata : Mr Om"));
+// })
+
 
 
 
